629ed6401681e402932d441012249dd5b7a436b8
Mark Kitchin <m930k318@home.ku.edu>
2023-10-22 23:48:05 -0500

    Basics of the Code

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+void readfile(int s, string filename)
+{
+	ifstream input(filename);
+	string line;
+	while (getline(input, line, '\n')) {
+		cout << line<<"\n";
+	}
+}
+
+void printmatrix()
+{
+	
+}
 
 int main() 
 {
-	cout << "Hello World";
+	cout << "Enter the name of the file.\n";
+	string filename;
+	cin >> filename;
+	ifstream input(filename);
+	string line;
+	getline(input, line, '\n');
+	string getsize = line;
+	const int size = stoi(getsize);
+	readfile(size, filename);
 	return 0;
 }
 
